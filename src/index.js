a3cb1fc3e6d6c5176aa6d36ef9004a5cbcc29c68
Vladislav Iskakov <71043551+Limabeth@users.noreply.github.com>
2021-10-12 03:58:38 +0800

    changed router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import 'react-app-polyfill/ie9';
+import "react-app-polyfill/ie9";
 
 import React from "react";
 import ReactDOM from "react-dom";
@@ -6,10 +6,13 @@ import ReactDOM from "react-dom";
 import "./styles/style.css";
 
 import App from "./App.js";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("app-root")
 );
